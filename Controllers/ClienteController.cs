8cfa63c21e818dedf8cb990f333dfa7d7a359ead
anyel-ec <paulangel826@gmail.com>
2024-03-04 07:50:14 -0500

    add new client and edit

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -32,6 +32,7 @@ namespace Lab2.Controllers
         }
         public ActionResult Nuevo()
         {
+           
             return View();
         }
 
@@ -58,6 +59,59 @@ namespace Lab2.Controllers
                     return Redirect("~/Cliente/Index");
                 }
                 return View (clienteViewModel);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+        ///////////////////////////////////////////////////////////////
+
+        public ActionResult Editar()
+        {
+            ClienteViewModel clienteViewModel = new ClienteViewModel();
+            using (lab2Entities db = new lab2Entities())
+            {
+                var cliente = db.cliente.Find(1);
+                clienteViewModel.nombre_cli = cliente.nombre_cli;
+                clienteViewModel.cedula_cli = cliente.cedula_cli;
+                clienteViewModel.correo = cliente.correo;
+                clienteViewModel.fechaNacimiento = (DateTime)cliente.fechaNacimiento;
+                clienteViewModel.id_cli = cliente.id_cli;
+            }
+            return View (clienteViewModel);
+
+
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Editar(ClienteViewModel clienteViewModel)
+        {
+            try
+            {
+                // valida los data Annotations
+                if (ModelState.IsValid)
+                {
+                    // si todo es valido se procede a guardar
+                    using (lab2Entities db = new lab2Entities())
+                    {
+                        var cliente = new cliente();
+                        cliente.nombre_cli = clienteViewModel.nombre_cli;
+                        cliente.cedula_cli = clienteViewModel.cedula_cli;
+                        cliente.correo = clienteViewModel.correo;
+                        cliente.fechaNacimiento = clienteViewModel.fechaNacimiento;
+
+                        db.cliente.Add(cliente);
+                        db.SaveChanges();
+                    }
+                    return Redirect("~/Cliente/Index");
+                }
+                return View(clienteViewModel);
             }
             catch (Exception ex)
             {
